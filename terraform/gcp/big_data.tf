42b7c1cd2bcf024c65f8f63b7bb2a27fe61cee73
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-06-14 20:01:58 +0530

    Harness AI Remediation to fix: Ensure all Cloud SQL database instance requires all incoming connections to use SSL

--- terraform/gcp/big_data.tf
+++ terraform/gcp/big_data.tf
@@ -11,10 +11,29 @@ resource "google_sql_database_instance" "master_instance" {
         name  = "WWW"
         value = "0.0.0.0/0"
       }
+      require_ssl = true
     }
     backup_configuration {
       enabled = false
     }
+  }
+}
+
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id = "terragoat_${var.environment}_dataset"
+  access {
+    special_group = "allAuthenticatedUsers"
+    role          = "READER"
+  }
+  labels = {
+    git_commit           = "2bdc0871a5f4505be58244029cc6485d45d7bb8e"
+    git_file             = "terraform__gcp__big_data_tf"
+    git_last_modified_at = "2022-01-19-17-02-27"
+    git_last_modified_by = "jameswoolfenden"
+    git_modifiers        = "jameswoolfenden__nimrodkor"
+    git_org              = "bridgecrewio"
+    git_repo             = "terragoat"
+    yor_trace            = "2560d883-bc3a-4cb6-b9fc-fb666edf626e"
   }
 }
 
