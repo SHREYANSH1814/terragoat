7906834a80019a33fefe3034877585393547d861
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-06-17 10:12:00 +0530

    Harness AI Remediation to fix: Ensure ECR image scanning on push is enabled

--- terraform/aws/ecr.tf
+++ terraform/aws/ecr.tf
@@ -1,4 +1,7 @@
 resource aws_ecr_repository "repository" {
+  image_scanning_configuration {
+    scan_on_push = true
+  }
   name                 = "${local.resource_prefix.value}-repository"
   image_tag_mutability = "MUTABLE"
 
@@ -15,6 +18,23 @@ resource aws_ecr_repository "repository" {
     git_repo             = "terragoat"
     yor_trace            = "7a3ec657-fa54-4aa2-8467-5d08d6c90bc2"
   })
+}
+
+locals {
+  docker_image = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com/${aws_ecr_repository.repository.name}"
+}
+
+
+resource null_resource "push_image" {
+  provisioner "local-exec" {
+    working_dir = "${path.module}/resources"
+    command     = <<BASH
+    aws ecr get-login-password --region ${var.region} | docker login --username AWS --password-stdin ${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com
+    docker build -t ${aws_ecr_repository.repository.name} .
+    docker tag ${aws_ecr_repository.repository.name} ${local.docker_image}
+    docker push ${local.docker_image}
+    BASH
+  }
 }
 
 locals {
