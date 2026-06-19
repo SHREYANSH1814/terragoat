93fb46d43c670a5a76a65cfbbb898b945cc49a0e
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-06-19 10:18:05 +0530

    Harness AI Remediation to fix: Ensure IAM policies does not allow write access without constraints

--- terraform/aws/db-app.tf
+++ terraform/aws/db-app.tf
@@ -213,12 +213,20 @@ resource "aws_iam_role_policy" "ec2policy" {
   "Statement": [
     {
       "Action": [
-        "s3:*",
-        "ec2:*",
-        "rds:*"
+        "s3:ListBucket",
+        "s3:GetObject",
+        "s3:PutObject",
+        "ec2:Describe*",
+        "rds:Describe*",
+        "rds:Connect"
       ],
       "Effect": "Allow",
-      "Resource": "*"
+      "Resource": [
+        "arn:aws:s3:::example-bucket",
+        "arn:aws:s3:::example-bucket/*",
+        "arn:aws:ec2:region:account-id:instance/*",
+        "arn:aws:rds:region:account-id:db:example-db-instance"
+      ]
     }
   ]
 }
