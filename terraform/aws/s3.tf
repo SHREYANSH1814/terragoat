ccf1930bf0ed3227b0b7f69464b789ad410ea4e0
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-07-06 15:13:33 +0530

    Harness AI Remediation to fix: Ensure the S3 bucket has access logging enabled

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -1,10 +1,15 @@
 resource "aws_s3_bucket" "data" {
   # bucket is public
   # bucket is not encrypted
-  # bucket does not have access logs
   # bucket does not have versioning
   bucket        = "${local.resource_prefix.value}-data"
   force_destroy = true
+
+  logging {
+    target_bucket = "${aws_s3_bucket.logs.id}"
+    target_prefix = "log/"
+  }
+
   tags = merge({
     Name        = "${local.resource_prefix.value}-data"
     Environment = local.resource_prefix.value
