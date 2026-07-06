0220f3258336a299e21bb3a24484959337ba2886
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-07-06 15:36:08 +0530

    Harness AI Remediation to fix: Ensure all data stored in Aurora is securely encrypted at rest

--- terraform/aws/rds.tf
+++ terraform/aws/rds.tf
@@ -98,6 +98,7 @@ resource "aws_rds_cluster" "app7-rds-cluster" {
   cluster_identifier      = "app7-rds-cluster"
   allocated_storage       = 10
   backup_retention_period = 25
+  storage_encrypted       = true
   tags = {
     git_commit           = "079fe74f6b96d887c245664fbd8cf676c92f20e5"
     git_file             = "terraform/aws/rds.tf"
