100b934d9918d5c85eab83c02205253ba9f60e99
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-07-06 15:12:55 +0530

    Harness AI Remediation to fix: Enable VPC Flow Logs and Intranode Visibility

--- terraform/gcp/gke.tf
+++ terraform/gcp/gke.tf
@@ -5,12 +5,12 @@ data "google_compute_zones" "available_zones" {
 
 resource "google_container_cluster" "workload_cluster" {
   name               = "terragoat-${var.environment}-cluster"
-  logging_service    = "none"
+  logging_service    = "logging.googleapis.com/kubernetes"
   location           = var.region
   initial_node_count = 1
 
   enable_legacy_abac       = true
-  monitoring_service       = "none"
+  monitoring_service       = "monitoring.googleapis.com/kubernetes"
   remove_default_node_pool = true
   network                  = google_compute_network.vpc.name
   subnetwork               = google_compute_subnetwork.public-subnetwork.name
@@ -19,6 +19,11 @@ resource "google_container_cluster" "workload_cluster" {
       cidr_block = "0.0.0.0/0"
     }
   }
+  ip_allocation_policy {
+    cluster_secondary_range_name  = google_compute_subnetwork.public-subnetwork.secondary_ip_range_names[0]
+    services_secondary_range_name = google_compute_subnetwork.public-subnetwork.secondary_ip_range_names[1]
+  }
+  enable_intranode_visibility = true
 }
 
 resource "google_container_node_pool" "custom_node_pool" {
