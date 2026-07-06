ee5c3b48e3cdae499203d15fb291c8ac7e45d408
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-07-06 14:27:39 +0530

    Harness AI Remediation to fix: Ensure IAM policies does not allow permissions management / resource exposure without constraints

--- terraform/aws/iam.tf
+++ terraform/aws/iam.tf
@@ -23,8 +23,8 @@ resource "aws_iam_access_key" "user" {
 }
 
 resource "aws_iam_user_policy" "userpolicy" {
-  name = "excess_policy"
-  user = "${aws_iam_user.user.name}"
+  name = "restricted_policy"
+  user = aws_iam_user.user.name
 
   policy = <<EOF
 {
@@ -32,13 +32,18 @@ resource "aws_iam_user_policy" "userpolicy" {
   "Statement": [
     {
       "Action": [
-        "ec2:*",
-        "s3:*",
-        "lambda:*",
-        "cloudwatch:*"
+        "ec2:Describe*",
+        "s3:GetObject",
+        "lambda:InvokeFunction",
+        "cloudwatch:GetMetricData"
       ],
       "Effect": "Allow",
-      "Resource": "*"
+      "Resource": [
+        "arn:aws:ec2:us-east-1:123456789012:instance/*",
+        "arn:aws:s3:::example-bucket/*",
+        "arn:aws:lambda:us-east-1:123456789012:function:example-function",
+        "arn:aws:cloudwatch:us-east-1:123456789012:alarm:example-alarm"
+      ]
     }
   ]
 }
