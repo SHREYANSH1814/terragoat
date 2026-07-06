24ff351a22bd2d840618817d7f4b8d36323aaa6f
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-07-06 15:13:45 +0530

    Harness AI Remediation to fix: Ensure audit profile captures all the activities

--- terraform/azure/logging.tf
+++ terraform/azure/logging.tf
@@ -1,6 +1,6 @@
 resource azurerm_monitor_log_profile "logging_profile" {
   storage_account_id = azurerm_storage_account.example.id
-  categories         = ["Action"]
+  categories         = ["Write", "Delete", "Action", "Delete", "Read"]
   locations          = [var.location]
   name               = "terragoat-${var.environment}"
   retention_policy {
