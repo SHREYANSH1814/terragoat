022a56a824b25d112032e156fef09cb510a38ad6
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-07-06 15:13:53 +0530

    Harness AI Remediation to fix: Ensure that App service enables failed request tracing

--- terraform/azure/app_service.tf
+++ terraform/azure/app_service.tf
@@ -47,6 +47,10 @@ resource azurerm_app_service "app-service2" {
   resource_group_name = azurerm_resource_group.example.name
   https_only          = true
 
+  site_config {
+    failed_request_tracing_enabled = true
+  }
+
   auth_settings {
     enabled = false
   }
