c663fb07de241b9582e0509970ac0a6961247b5d
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-07-06 15:12:50 +0530

    Harness AI Remediation to fix: Ensure that 'Threat Detection types' is set to 'All'

--- terraform/azure/mssql.tf
+++ terraform/azure/mssql.tf
@@ -198,10 +198,7 @@ resource "azurerm_mssql_server_security_alert_policy" "alertpolicy4" {
   state                      = "Enabled"
   storage_endpoint           = azurerm_storage_account.security_storage_account.primary_blob_endpoint
   storage_account_access_key = azurerm_storage_account.security_storage_account.primary_access_key
-  disabled_alerts = [
-    "Sql_Injection",
-    "Data_Exfiltration"
-  ]
+  disabled_alerts = []
   retention_days  = 20
   email_addresses = ["securityengineer@bridgecrew.io"]
 }
@@ -212,10 +209,7 @@ resource "azurerm_mssql_server_security_alert_policy" "alertpolicy5" {
   state                      = "Enabled"
   storage_endpoint           = azurerm_storage_account.security_storage_account.primary_blob_endpoint
   storage_account_access_key = azurerm_storage_account.security_storage_account.primary_access_key
-  disabled_alerts = [
-    "Sql_Injection",
-    "Data_Exfiltration"
-  ]
+  disabled_alerts = []
   retention_days = 20
 }
 
@@ -225,10 +219,7 @@ resource "azurerm_mssql_server_security_alert_policy" "alertpolicy6" {
   state                      = "Enabled"
   storage_endpoint           = azurerm_storage_account.security_storage_account.primary_blob_endpoint
   storage_account_access_key = azurerm_storage_account.security_storage_account.primary_access_key
-  disabled_alerts = [
-    "Sql_Injection",
-    "Data_Exfiltration"
-  ]
+  disabled_alerts = []
   retention_days  = 20
   email_addresses = ["securityengineer@bridgecrew.io"]
 }
@@ -239,10 +230,7 @@ resource "azurerm_mssql_server_security_alert_policy" "alertpolicy7" {
   state                      = "Enabled"
   storage_endpoint           = azurerm_storage_account.security_storage_account.primary_blob_endpoint
   storage_account_access_key = azurerm_storage_account.security_storage_account.primary_access_key
-  disabled_alerts = [
-    "Sql_Injection",
-    "Data_Exfiltration"
-  ]
+  disabled_alerts = []
   retention_days  = 20
   email_addresses = ["securityengineer@bridgecrew.io"]
 }
