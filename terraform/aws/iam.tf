9e481203ed28f4b1dde7b1d46a9ebcca42c9db1e
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-07-06 15:12:31 +0530

    Harness AI Remediation to fix: Ensure IAM policies does not allow credentials exposure

--- terraform/aws/iam.tf
+++ terraform/aws/iam.tf
@@ -23,21 +23,21 @@ resource "aws_iam_access_key" "user" {
 }
 
 resource "aws_iam_user_policy" "userpolicy" {
-  name = "excess_policy"
-  user = "${aws_iam_user.user.name}"
+  name = "restricted_policy"
+  user = aws_iam_user.user.name
 
   policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Action": [
-        "ec2:*",
-        "s3:*",
-        "lambda:*",
-        "cloudwatch:*"
-      ],
       "Effect": "Allow",
+      "Action": [
+        "ec2:Describe*",
+        "s3:ListBucket",
+        "lambda:ListFunctions",
+        "cloudwatch:GetMetricData"
+      ],
       "Resource": "*"
     }
   ]
