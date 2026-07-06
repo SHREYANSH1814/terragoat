371496390e71caa2647569e19e90baedbae5993a
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-07-06 15:02:52 +0530

    Harness AI Remediation to fix: Ensure IAM policies does not allow permissions management / resource exposure without constraints

--- terraform/aws/db-app.tf
+++ terraform/aws/db-app.tf
@@ -213,12 +213,17 @@ resource "aws_iam_role_policy" "ec2policy" {
   "Statement": [
     {
       "Action": [
-        "s3:*",
-        "ec2:*",
-        "rds:*"
+        "s3:ListBucket",
+        "s3:GetObject",
+        "ec2:DescribeInstances",
+        "rds:DescribeDBInstances"
       ],
       "Effect": "Allow",
-      "Resource": "*"
+      "Resource": [
+        "arn:aws:s3:::example-bucket",
+        "arn:aws:s3:::example-bucket/*",
+        "*"
+      ]
     }
   ]
 }
