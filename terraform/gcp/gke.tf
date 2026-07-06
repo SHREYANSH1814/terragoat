d5fca96f613a1ccb87a9f8de66eaf4c31aef1416
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-07-06 15:13:46 +0530

    Harness AI Remediation to fix: Ensure client certificate authentication to Kubernetes Engine Clusters is disabled

--- terraform/gcp/gke.tf
+++ terraform/gcp/gke.tf
@@ -9,7 +9,7 @@ resource "google_container_cluster" "workload_cluster" {
   location           = var.region
   initial_node_count = 1
 
-  enable_legacy_abac       = true
+  enable_legacy_abac       = false
   monitoring_service       = "none"
   remove_default_node_pool = true
   network                  = google_compute_network.vpc.name
@@ -17,6 +17,11 @@ resource "google_container_cluster" "workload_cluster" {
   master_authorized_networks_config {
     cidr_blocks {
       cidr_block = "0.0.0.0/0"
+    }
+  }
+  master_auth {
+    client_certificate_config {
+      issue_client_certificate = false
     }
   }
 }
