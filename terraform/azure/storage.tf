46152a7530fb807fbe36ae0c2f8a75003c70de76
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-07-06 15:13:13 +0530

    Harness AI Remediation to fix: Ensure Storage Account is using the latest version of TLS encryption

--- terraform/azure/storage.tf
+++ terraform/azure/storage.tf
@@ -26,6 +26,7 @@ resource "azurerm_storage_account" "example" {
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
+  min_tls_version          = "TLS1_2"
   queue_properties {
     logging {
       delete                = false
