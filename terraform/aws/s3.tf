652540a6c26d61f1ae9aaaa28b3f9d4424d98e15
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-07-06 15:13:17 +0530

    Harness AI Remediation to fix: Ensure all data stored in the S3 bucket have versioning enabled

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -42,10 +42,12 @@ resource "aws_s3_bucket_object" "data_object" {
 resource "aws_s3_bucket" "financials" {
   # bucket is not encrypted
   # bucket does not have access logs
-  # bucket does not have versioning
   bucket        = "${local.resource_prefix.value}-financials"
   acl           = "private"
   force_destroy = true
+  versioning {
+    enabled = true
+  }
   tags = merge({
     Name        = "${local.resource_prefix.value}-financials"
     Environment = local.resource_prefix.value
