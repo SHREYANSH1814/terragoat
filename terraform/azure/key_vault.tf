172031b7f70fadbd0e75db281aa2c1adb97eb05c
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-07-06 15:14:14 +0530

    Harness AI Remediation to fix: Ensure that key vault secrets have "content_type" set

--- terraform/azure/key_vault.tf
+++ terraform/azure/key_vault.tf
@@ -59,6 +59,7 @@ resource "azurerm_key_vault_secret" "secret" {
   key_vault_id = azurerm_key_vault.example.id
   name         = "terragoat-secret-${var.environment}"
   value        = random_string.password.result
+  content_type = "text/plain"
   tags = {
     git_commit           = "f8ff847bb69370bbe03b3d2b70db586ff6c867fc"
     git_file             = "terraform/azure/key_vault.tf"
