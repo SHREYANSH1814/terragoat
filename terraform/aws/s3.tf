87b4ea50744a96f06dfd1ebf60568bdcb7d6faa9
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-07-06 15:14:09 +0530

    Harness AI Remediation to fix: Ensure that S3 buckets are encrypted with KMS by default

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -87,7 +87,7 @@ resource "aws_s3_bucket" "operations" {
 }
 
 resource "aws_s3_bucket" "data_science" {
-  # bucket is not encrypted
+  # bucket is encrypted with KMS
   bucket = "${local.resource_prefix.value}-data-science"
   acl    = "private"
   versioning {
@@ -96,6 +96,14 @@ resource "aws_s3_bucket" "data_science" {
   logging {
     target_bucket = "${aws_s3_bucket.logs.id}"
     target_prefix = "log/"
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "aws:kms"
+        kms_master_key_id = "${aws_kms_key.data_science_key.arn}"
+      }
+    }
   }
   force_destroy = true
   tags = {
