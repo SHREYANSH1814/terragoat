834ac2b820c6109ca60ea708ec60aceedd8ce109
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-07-06 15:13:45 +0530

    Harness AI Remediation to fix: Ensure all data stored in the Elasticsearch is securely encrypted at rest

--- terraform/aws/es.tf
+++ terraform/aws/es.tf
@@ -10,10 +10,12 @@ resource "aws_elasticsearch_domain" "monitoring-framework" {
     dedicated_master_count   = 1
   }
 
-
   ebs_options {
     ebs_enabled = true
     volume_size = 30
+    encrypt_at_rest {
+      enabled = true
+    }
   }
   tags = {
     git_commit           = "e6d83b21346fe85d4fe28b16c0b2f1e0662eb1d7"
