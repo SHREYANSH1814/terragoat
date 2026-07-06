b2e55e7055f24bef084ed047b98c9f66adda65ad
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-07-06 15:13:01 +0530

    Harness AI Remediation to fix: Ensure Azure AD authentication is enabled for Azure SQL (MSSQL)

--- terraform/azure/mssql.tf
+++ terraform/azure/mssql.tf
@@ -81,6 +81,10 @@ resource "azurerm_mssql_server" "mssql4" {
   version                      = "12.0"
   administrator_login          = "missadministrator"
   administrator_login_password = "AdminPassword123!"
+  azure_ad_administrator {
+    login = "aadadminuser"
+    object_id = var.azure_ad_admin_object_id
+  }
   tags = {
     git_commit           = "c6f8caa51942284d02465518822685897ad90141"
     git_file             = "terraform/azure/mssql.tf"
