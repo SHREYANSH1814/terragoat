19835d410b947d03c63e999461757a9a1d23cafa
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-07-06 15:13:52 +0530

    Harness AI Remediation to fix: Ensure that App Service configures health check

--- terraform/azure/app_service.tf
+++ terraform/azure/app_service.tf
@@ -47,6 +47,10 @@ resource azurerm_app_service "app-service2" {
   resource_group_name = azurerm_resource_group.example.name
   https_only          = true
 
+  site_config {
+    health_check_path = "/health"
+  }
+
   auth_settings {
     enabled = false
   }
