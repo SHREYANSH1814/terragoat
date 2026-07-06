5451159c215c7337b3247dc0dec5dd03d88e2629
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-07-06 15:13:22 +0530

    Harness AI Remediation to fix: Ensure that 'Email service and co-administrators' is 'Enabled' for MSSQL servers

--- terraform/azure/mssql.tf
+++ terraform/azure/mssql.tf
@@ -217,6 +217,8 @@ resource "azurerm_mssql_server_security_alert_policy" "alertpolicy5" {
     "Data_Exfiltration"
   ]
   retention_days = 20
+  email_addresses = ["securityengineer@bridgecrew.io"]
+  email_account_admins = true
 }
 
 resource "azurerm_mssql_server_security_alert_policy" "alertpolicy6" {
