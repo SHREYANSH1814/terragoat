6557218a28b5f572153841f83c54979f9a470569
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-07-06 15:13:32 +0530

    Harness AI Remediation to fix: Ensure the GKE Release Channel is set

--- terraform/gcp/gke.tf
+++ terraform/gcp/gke.tf
@@ -19,6 +19,10 @@ resource "google_container_cluster" "workload_cluster" {
       cidr_block = "0.0.0.0/0"
     }
   }
+
+  release_channel {
+    channel = "REGULAR"
+  }
 }
 
 resource "google_container_node_pool" "custom_node_pool" {
