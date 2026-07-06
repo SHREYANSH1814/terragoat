5281a3cb87afe281e4ae61737c4e0be4c091607b
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-07-06 15:13:11 +0530

    Harness AI Remediation to fix: Ensure RDS cluster has IAM authentication enabled

--- terraform/aws/rds.tf
+++ terraform/aws/rds.tf
@@ -34,6 +34,7 @@ resource "aws_rds_cluster" "app3-rds-cluster" {
   cluster_identifier      = "app3-rds-cluster"
   allocated_storage       = 10
   backup_retention_period = 15
+  iam_database_authentication_enabled = true
   tags = {
     git_commit           = "079fe74f6b96d887c245664fbd8cf676c92f20e5"
     git_file             = "terraform/aws/rds.tf"
