358db478d7c82164b345d65b1ec1eb02a5beaf93
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-07-06 15:03:04 +0530

    Harness AI Remediation to fix: Ensure that 'Send Alerts To' is enabled for MSSQL servers

--- terraform/azure/sql.tf
+++ terraform/azure/sql.tf
@@ -32,6 +32,8 @@ resource "azurerm_mssql_server_security_alert_policy" "example" {
   resource_group_name        = azurerm_resource_group.example.name
   server_name                = azurerm_sql_server.example.name
   state                      = "Enabled"
+  email_account_admins       = true
+  email_addresses            = ["admin@example.com"]
   storage_endpoint           = azurerm_storage_account.example.primary_blob_endpoint
   storage_account_access_key = azurerm_storage_account.example.primary_access_key
   disabled_alerts = [
