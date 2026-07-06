398284a06c9d27016d42e60fcd9360328fff3d91
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-07-06 15:13:48 +0530

    Harness AI Remediation to fix: Ensure every security group and rule has a description

--- terraform/aws/db-app.tf
+++ terraform/aws/db-app.tf
@@ -149,6 +149,7 @@ resource "aws_security_group_rule" "egress" {
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = "${aws_security_group.default.id}"
+  description       = "Allow all outbound traffic"
 }
 
 
