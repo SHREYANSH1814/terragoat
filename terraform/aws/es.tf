8265a55eaac4272da6b0338d8e7582281ad09e3b
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-07-06 15:13:01 +0530

    Harness AI Remediation to fix: Ensure Elasticsearch Domain Audit Logging is enabled

--- terraform/aws/es.tf
+++ terraform/aws/es.tf
@@ -10,11 +10,16 @@ resource "aws_elasticsearch_domain" "monitoring-framework" {
     dedicated_master_count   = 1
   }
 
-
   ebs_options {
     ebs_enabled = true
     volume_size = 30
   }
+
+  log_publishing_options {
+    cloudwatch_log_group_arn = aws_cloudwatch_log_group.es_audit_logs.arn
+    log_type                 = "AUDIT_LOGS"
+  }
+
   tags = {
     git_commit           = "e6d83b21346fe85d4fe28b16c0b2f1e0662eb1d7"
     git_file             = "terraform/aws/es.tf"
@@ -26,6 +31,12 @@ resource "aws_elasticsearch_domain" "monitoring-framework" {
     yor_trace            = "95131dec-d7c9-49bb-9aff-eb0e2736603b"
   }
 }
+
+resource "aws_cloudwatch_log_group" "es_audit_logs" {
+  name              = "/aws/es/audit-logs/${aws_elasticsearch_domain.monitoring-framework.domain_name}"
+  retention_in_days = 30
+}
+
 
 data aws_iam_policy_document "policy" {
   statement {
