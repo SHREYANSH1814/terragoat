3bd79f6ccd29a123030552c39e95939fec77cada
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-07-06 15:13:02 +0530

    Harness AI Remediation to fix: Ensure FTP deployments are disabled

--- terraform/azure/app_service.tf
+++ terraform/azure/app_service.tf
@@ -27,6 +27,7 @@ resource azurerm_app_service "app-service1" {
   https_only          = false
   site_config {
     min_tls_version = "1.1"
+    ftps_state     = "Disabled"
   }
   tags = {
     git_commit           = "81738b80d571fa3034633690d13ffb460e1e7dea"
