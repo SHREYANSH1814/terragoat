31671579b9387bdea47973b7ec88fb6191e0248b
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-07-06 15:13:13 +0530

    Harness AI Remediation to fix: Ensure Storage logging is enabled for Queue service for read, write and delete requests

--- terraform/azure/mssql.tf
+++ terraform/azure/mssql.tf
@@ -5,6 +5,15 @@ resource "azurerm_storage_account" "security_storage_account" {
   account_tier              = "Standard"
   account_replication_type  = "LRS"
   enable_https_traffic_only = true
+
+  logging {
+    delete                 = true
+    read                   = true
+    write                  = true
+    version                = "1.0"
+    retention_policy_days  = 7
+  }
+
   tags = {
     git_commit           = "a1d1c1ce31a1bde6dafa188846d90eca82abe5fd"
     git_file             = "terraform/azure/mssql.tf"
