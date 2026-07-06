78df52cecc1c759fc2ba2db8d1ddcee823fa68a6
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-07-06 15:13:33 +0530

    Harness AI Remediation to fix: Ensure the S3 bucket has access logging enabled

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -272,6 +272,11 @@ resource "aws_s3_bucket" "flowbucket" {
   bucket        = "${local.resource_prefix.value}-flowlogs"
   force_destroy = true
 
+  logging {
+    target_bucket = "${local.resource_prefix.value}-flowlogs-logs"
+    target_prefix = "log/"
+  }
+
   tags = merge({
     Name        = "${local.resource_prefix.value}-flowlogs"
     Environment = local.resource_prefix.value
@@ -284,6 +289,27 @@ resource "aws_s3_bucket" "flowbucket" {
     git_org              = "bridgecrewio"
     git_repo             = "terragoat"
     yor_trace            = "f058838a-b1e0-4383-b965-7e06e987ffb1"
+  })
+}
+
+resource "aws_s3_bucket" "flowbucket_logs" {
+  bucket        = "${local.resource_prefix.value}-flowlogs-logs"
+  force_destroy = true
+
+  acl = "log-delivery-write"
+
+  tags = merge({
+    Name        = "${local.resource_prefix.value}-flowlogs-logs"
+    Environment = local.resource_prefix.value
+    }, {
+    git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
+    git_file             = "terraform/aws/ec2.tf"
+    git_last_modified_at = "2020-06-16 14:46:24"
+    git_last_modified_by = "nimrodkor@gmail.com"
+    git_modifiers        = "nimrodkor"
+    git_org              = "bridgecrewio"
+    git_repo             = "terragoat"
+    yor_trace            = "new-log-bucket"
   })
 }
 
