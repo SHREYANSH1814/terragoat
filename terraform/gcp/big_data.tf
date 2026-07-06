ead76c183aa2ac7aa4974cbcf465c6eece2fe9d1
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-07-06 15:13:37 +0530

    Harness AI Remediation to fix: Ensure PostgreSQL database 'log_connections' flag is set to 'on'

--- terraform/gcp/big_data.tf
+++ terraform/gcp/big_data.tf
@@ -15,6 +15,10 @@ resource "google_sql_database_instance" "master_instance" {
     backup_configuration {
       enabled = false
     }
+    database_flags {
+      name  = "log_connections"
+      value = "on"
+    }
   }
 }
 
