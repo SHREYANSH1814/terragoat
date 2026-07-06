9e345bfa22fecc9b1644013fc3be1bc4eb36a447
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-07-06 15:13:35 +0530

    Harness AI Remediation to fix: Ensure the web app has 'Client Certificates (Incoming client certificates)' set

--- terraform/azure/app_service.tf
+++ terraform/azure/app_service.tf
@@ -24,9 +24,10 @@ resource azurerm_app_service "app-service1" {
   location            = var.location
   name                = "terragoat-app-service-${var.environment}${random_integer.rnd_int.result}"
   resource_group_name = azurerm_resource_group.example.name
-  https_only          = false
+  https_only          = true
   site_config {
     min_tls_version = "1.1"
+    client_cert_enabled = true
   }
   tags = {
     git_commit           = "81738b80d571fa3034633690d13ffb460e1e7dea"
