a5803b2d13cab4e0a3914d70ef5c210da24e6ddd
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-07-06 15:12:43 +0530

    Harness AI Remediation to fix: Ensure all data stored in the Launch configuration or instance Elastic Blocks Store is securely encrypted

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -32,9 +32,8 @@ EOF
 }
 
 resource "aws_ebs_volume" "web_host_storage" {
-  # unencrypted volume
   availability_zone = "${var.region}a"
-  #encrypted         = false  # Setting this causes the volume to be recreated on apply 
+  encrypted         = true
   size = 1
   tags = merge({
     Name = "${local.resource_prefix.value}-ebs"
@@ -51,9 +50,9 @@ resource "aws_ebs_volume" "web_host_storage" {
 }
 
 resource "aws_ebs_snapshot" "example_snapshot" {
-  # ebs snapshot without encryption
   volume_id   = "${aws_ebs_volume.web_host_storage.id}"
   description = "${local.resource_prefix.value}-ebs-snapshot"
+  encrypted   = true
   tags = merge({
     Name = "${local.resource_prefix.value}-ebs-snapshot"
     }, {
