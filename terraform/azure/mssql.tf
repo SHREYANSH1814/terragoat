ef372b644fdf4588c7e613a12e2454106f495dc4
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-07-06 15:12:49 +0530

    Harness AI Remediation to fix: Ensure that 'Threat Detection types' is set to 'All'

--- terraform/azure/mssql.tf
+++ terraform/azure/mssql.tf
@@ -239,10 +239,8 @@ resource "azurerm_mssql_server_security_alert_policy" "alertpolicy7" {
   state                      = "Enabled"
   storage_endpoint           = azurerm_storage_account.security_storage_account.primary_blob_endpoint
   storage_account_access_key = azurerm_storage_account.security_storage_account.primary_access_key
-  disabled_alerts = [
-    "Sql_Injection",
-    "Data_Exfiltration"
-  ]
+  disabled_alerts = []
   retention_days  = 20
   email_addresses = ["securityengineer@bridgecrew.io"]
+  threat_detection_types = ["All"]
 }
