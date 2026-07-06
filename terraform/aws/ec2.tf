9a1b8a521968855a5e5e5720094434aa1e208a2a
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-07-06 15:12:43 +0530

    Harness AI Remediation to fix: Ensure all data stored in the EBS is securely encrypted

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -32,10 +32,9 @@ EOF
 }
 
 resource "aws_ebs_volume" "web_host_storage" {
-  # unencrypted volume
   availability_zone = "${var.region}a"
-  #encrypted         = false  # Setting this causes the volume to be recreated on apply 
-  size = 1
+  encrypted         = true
+  size              = 1
   tags = merge({
     Name = "${local.resource_prefix.value}-ebs"
     }, {
