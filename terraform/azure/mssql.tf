67e496a043bf76c5d71743420e1924a01e3764e1
SHREYANSH GUPTA <101034768+SHREYANSH1814@users.noreply.github.com>
2026-07-06 15:13:06 +0530

    Harness AI Remediation to fix: Ensure MSSQL is using the latest version of TLS encryption

--- terraform/azure/mssql.tf
+++ terraform/azure/mssql.tf
@@ -119,6 +119,7 @@ resource "azurerm_mssql_server" "mssql6" {
   version                      = "12.0"
   administrator_login          = "missadministrator"
   administrator_login_password = "AdminPassword123!"
+  minimal_tls_version          = "1.2"
   tags = {
     git_commit           = "c6f8caa51942284d02465518822685897ad90141"
     git_file             = "terraform/azure/mssql.tf"
